960a48fe700ff00962eede86e5d476300445ab03
GabrielScarpelin <gabriel.scaarpelin319@outlook.com>
2023-06-04 20:28:05 -0300

    Up servidor nlw

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
+
+const app = fastify()
+app.register(cors, {
+  origin: true,
+})
+app.register(jwt, {
+  secret: 'spacetime',
+})
+app.register(memoriesRoutes)
+app.register(authRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then((value) => {
+    console.log('Server Running...')
+  })
